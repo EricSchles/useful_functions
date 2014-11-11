233061d47aac39eb246bb7ac8d63c14f8c5b1e20
EricSchles <ericschles@gmail.com>
2014-11-11 05:56:44 -0800

    started ordering function

--- useful.py
+++ useful.py
@@ -2,3 +2,9 @@ def remove_all(listing,x):
     while x in listing:
         listing.remove(x)
     return listing
+
+def impose_ordering(ordering,listing):
+    tmp = [x for x in listing if x in ordering]
+    new_listing = []
+    for elem in ordering:
+        
