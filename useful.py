f163a0b716a75d5876781a5c9b35026b17c7b5a7
EricSchles <ericschles@gmail.com>
2014-11-14 12:52:28 -0800

    third attempt - algorithm has been fleshed out but needs to be implemented

--- useful.py
+++ useful.py
@@ -67,5 +67,23 @@ def fuzzer_by_whitespace(text):
             final.append(i)
     return final
 
+def fuzzer3_by_whitespace(text):
+    """
+    algorithm:
+    start with the original string
+    add spaces between each letter
+    start at the last space and remove them one by one
+    then decrease the number of spaces by one from the front and repeat previous step
+    remove any duplicates along the way
+    """
+    characters = [x for x in text]
+    total_num_spaces = len(characters) #it's -2 because we leave off the ends
+    full_spaces = []
+    for ind,elem in enumerate(characters):
+        if ind != 0:
+            full_spaces.append(' ')
+        full_spaces.append(elem)
+
+
 for i in fuzz_by_whitespace("Hello"):
     print i
